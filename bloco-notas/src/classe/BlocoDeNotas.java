176b6b66fa5444604129f5074219b5de15822bba
nixcodedev <nixon.codedev@gmail.com>
2023-08-17 17:40:39 -0300

    Editando comentários

--- bloco-notas/src/classe/BlocoDeNotas.java
+++ bloco-notas/src/classe/BlocoDeNotas.java
@@ -23,227 +23,229 @@ import javax.swing.KeyStroke;
 
 public class BlocoDeNotas {
 
-    private static JFrame frame;
-    private static JTextArea textArea;
+	private static JFrame frame;
+	private static JTextArea textArea;
 
-    public static void main(String[] args) {
-        frame = new JFrame("Bloco de Notas");
-        frame.setLocationRelativeTo(null);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("Notes by NixCodeDev");
+	public static void main(String[] args) {
+		frame = new JFrame("Notes by NixCodeDev");
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        /*Área de texto*/
-        textArea = new JTextArea();
-        JScrollPane scrollPane = new JScrollPane(textArea);
-        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
-        scrollPane.setPreferredSize(new java.awt.Dimension(400, 300));
-        frame.getContentPane().add(scrollPane);
+		/* Área de texto */
+		textArea = new JTextArea();
+		JScrollPane scrollPane = new JScrollPane(textArea);
+		scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+		scrollPane.setPreferredSize(new java.awt.Dimension(400, 300));
+		frame.getContentPane().add(scrollPane);
 
-        /*Barra de Menus*/
-        JMenuBar menuBar = new JMenuBar();
-        frame.setJMenuBar(menuBar);
-        
-        /*Menu Arquivo*/
-        JMenu arquivoMenu = new JMenu("Arquivo");
-        menuBar.add(arquivoMenu);
-        
-        /*Ítens do Menu Arquivo*/
-        JMenuItem novoItem = new JMenuItem("Novo");
-        novoItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, KeyEvent.CTRL_DOWN_MASK));
-        arquivoMenu.add(novoItem);
-        
-        JMenuItem abrirItem = new JMenuItem("Abrir", new ImageIcon("caminho/do/icone/open.png"));
-        abrirItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, KeyEvent.CTRL_DOWN_MASK));
-        arquivoMenu.add(abrirItem);
+		/* Barra de Menus */
+		JMenuBar menuBar = new JMenuBar();
+		frame.setJMenuBar(menuBar);
 
-        JMenuItem salvarItem = new JMenuItem("Salvar", new ImageIcon("caminho/do/icone/save.png"));
-        salvarItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, KeyEvent.CTRL_DOWN_MASK));
-        arquivoMenu.add(salvarItem);
+		/* Menu Arquivo */
+		JMenu arquivoMenu = new JMenu("Arquivo");
+		menuBar.add(arquivoMenu);
 
-        JMenuItem fecharArquivoItem = new JMenuItem("Fechar Arquivo");
-        fecharArquivoItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_W, KeyEvent.CTRL_DOWN_MASK));
-        arquivoMenu.add(fecharArquivoItem);
+		/* Ítens do Menu Arquivo */
+		JMenuItem novoItem = new JMenuItem("Novo");
+		novoItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, KeyEvent.CTRL_DOWN_MASK));
+		arquivoMenu.add(novoItem);
 
-        JMenuItem fecharProgramaItem = new JMenuItem("Fechar");
-        fecharProgramaItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F, KeyEvent.CTRL_DOWN_MASK));
-        arquivoMenu.add(fecharProgramaItem);
+		JMenuItem abrirItem = new JMenuItem("Abrir", new ImageIcon("caminho/do/icone/open.png"));
+		abrirItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, KeyEvent.CTRL_DOWN_MASK));
+		arquivoMenu.add(abrirItem);
 
-        /*Menu Editar*/
-        JMenu editarMenu = new JMenu("Editar");
-        menuBar.add(editarMenu);
-        
-        /*Ítens do Menu Editar*/
-        JMenuItem copiarItem = new JMenuItem("Copiar");
-        copiarItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, KeyEvent.CTRL_DOWN_MASK));
-        editarMenu.add(copiarItem);
+		JMenuItem salvarItem = new JMenuItem("Salvar", new ImageIcon("caminho/do/icone/save.png"));
+		salvarItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, KeyEvent.CTRL_DOWN_MASK));
+		arquivoMenu.add(salvarItem);
 
-        JMenuItem colarItem = new JMenuItem("Colar");
-        colarItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_V, KeyEvent.CTRL_DOWN_MASK));
-        editarMenu.add(colarItem);
+		JMenuItem fecharArquivoItem = new JMenuItem("Fechar Arquivo");
+		fecharArquivoItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_W, KeyEvent.CTRL_DOWN_MASK));
+		arquivoMenu.add(fecharArquivoItem);
 
-        JMenuItem localizarItem = new JMenuItem("Localizar");
-        localizarItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_L, KeyEvent.CTRL_DOWN_MASK));
-        editarMenu.add(localizarItem);
+		JMenuItem fecharProgramaItem = new JMenuItem("Fechar");
+		fecharProgramaItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F, KeyEvent.CTRL_DOWN_MASK));
+		arquivoMenu.add(fecharProgramaItem);
 
-        novoItem.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                int option = JOptionPane.showConfirmDialog(frame, "Deseja salvar o arquivo atual antes de criar um novo?",
-                        "Salvar", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE);
+		/* Menu Editar */
+		JMenu editarMenu = new JMenu("Editar");
+		menuBar.add(editarMenu);
 
-                if (option == JOptionPane.YES_OPTION) {
-                    salvarArquivo();
-                    criarNovoArquivo();
-                } else if (option == JOptionPane.NO_OPTION) {
-                    criarNovoArquivo();
-                }
-            }
-        });
+		/* Ítens do Menu Editar */
+		JMenuItem copiarItem = new JMenuItem("Copiar");
+		copiarItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, KeyEvent.CTRL_DOWN_MASK));
+		editarMenu.add(copiarItem);
 
-        salvarItem.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                salvarArquivo();
-            }
-        });
+		JMenuItem colarItem = new JMenuItem("Colar");
+		colarItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_V, KeyEvent.CTRL_DOWN_MASK));
+		editarMenu.add(colarItem);
 
-        abrirItem.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                int option = JOptionPane.showConfirmDialog(frame, "Deseja salvar o arquivo atual antes de abrir um novo?",
-                        "Salvar", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE);
+		JMenuItem localizarItem = new JMenuItem("Localizar");
+		localizarItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_L, KeyEvent.CTRL_DOWN_MASK));
+		editarMenu.add(localizarItem);
 
-                if (option == JOptionPane.YES_OPTION) {
-                    salvarArquivo();
-                    abrirArquivo();
-                } else if (option == JOptionPane.NO_OPTION) {
-                    abrirArquivo();
-                }
-            }
-        });
+		/* Ações dos ítens dos Menus */
+		novoItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int option = JOptionPane.showConfirmDialog(frame,
+						"Deseja salvar o arquivo atual antes de criar um novo?", "Salvar",
+						JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE);
 
-        fecharArquivoItem.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                int option = JOptionPane.showConfirmDialog(frame, "Deseja salvar o arquivo antes de fechá-lo?",
-                        "Salvar", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE);
+				if (option == JOptionPane.YES_OPTION) {
+					salvarArquivo();
+					criarNovoArquivo();
+				} else if (option == JOptionPane.NO_OPTION) {
+					criarNovoArquivo();
+				}
+			}
+		});
 
-                if (option == JOptionPane.YES_OPTION) {
-                    salvarArquivo();
-                    limparTextArea();
-                } else if (option == JOptionPane.NO_OPTION) {
-                    limparTextArea();
-                }
-            }
-        });
+		salvarItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				salvarArquivo();
+			}
+		});
 
-        fecharProgramaItem.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                fecharPrograma();
-            }
-        });
+		abrirItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int option = JOptionPane.showConfirmDialog(frame,
+						"Deseja salvar o arquivo atual antes de abrir um novo?", "Salvar",
+						JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE);
 
-        copiarItem.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                textArea.copy();
-            }
-        });
+				if (option == JOptionPane.YES_OPTION) {
+					salvarArquivo();
+					abrirArquivo();
+				} else if (option == JOptionPane.NO_OPTION) {
+					abrirArquivo();
+				}
+			}
+		});
 
-        colarItem.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                textArea.paste();
-            }
-        });
+		fecharArquivoItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int option = JOptionPane.showConfirmDialog(frame, "Deseja salvar o arquivo antes de fechá-lo?",
+						"Salvar", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE);
 
-        localizarItem.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                String searchString = JOptionPane.showInputDialog(frame, "Digite o texto a ser localizado:");
-                String text = textArea.getText();
+				if (option == JOptionPane.YES_OPTION) {
+					salvarArquivo();
+					limparTextArea();
+				} else if (option == JOptionPane.NO_OPTION) {
+					limparTextArea();
+				}
+			}
+		});
 
-                int index = text.indexOf(searchString);
-                if (index >= 0) {
-                    textArea.requestFocus();
-                    textArea.select(index, index + searchString.length());
-                } else {
-                    JOptionPane.showMessageDialog(frame, "Texto não encontrado.", "Localizar",
-                            JOptionPane.INFORMATION_MESSAGE);
-                }
-            }
-        });
+		fecharProgramaItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				fecharPrograma();
+			}
+		});
 
-        frame.pack();
-        frame.setVisible(true);
-    }
+		copiarItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textArea.copy();
+			}
+		});
 
-    private static void salvarArquivo() {
-        JFileChooser fileChooser = new JFileChooser();
-        int result = fileChooser.showSaveDialog(frame);
+		colarItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textArea.paste();
+			}
+		});
 
-        if (result == JFileChooser.APPROVE_OPTION) {
-            File file = fileChooser.getSelectedFile();
-            String filePath = file.getAbsolutePath();
+		localizarItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String searchString = JOptionPane.showInputDialog(frame, "Digite o texto a ser localizado:");
+				String text = textArea.getText();
 
-            try {
-                FileWriter fileWriter = new FileWriter(filePath);
-                BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+				int index = text.indexOf(searchString);
+				if (index >= 0) {
+					textArea.requestFocus();
+					textArea.select(index, index + searchString.length());
+				} else {
+					JOptionPane.showMessageDialog(frame, "Texto não encontrado.", "Localizar",
+							JOptionPane.INFORMATION_MESSAGE);
+				}
+			}
+		});
 
-                bufferedWriter.write(textArea.getText());
+		frame.pack();
+		frame.setVisible(true);
+	}
 
-                bufferedWriter.close();
-                fileWriter.close();
+	private static void salvarArquivo() {
+		JFileChooser fileChooser = new JFileChooser();
+		int result = fileChooser.showSaveDialog(frame);
 
-                JOptionPane.showMessageDialog(frame, "Arquivo salvo com sucesso!");
-            } catch (IOException ex) {
-                JOptionPane.showMessageDialog(frame, "Ocorreu um erro ao salvar o arquivo.", "Erro",
-                        JOptionPane.ERROR_MESSAGE);
-            }
-        }
-    }
+		if (result == JFileChooser.APPROVE_OPTION) {
+			File file = fileChooser.getSelectedFile();
+			String filePath = file.getAbsolutePath();
 
-    private static void abrirArquivo() {
-        JFileChooser fileChooser = new JFileChooser();
-        int result = fileChooser.showOpenDialog(frame);
+			try {
+				FileWriter fileWriter = new FileWriter(filePath);
+				BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
 
-        if (result == JFileChooser.APPROVE_OPTION) {
-            File file = fileChooser.getSelectedFile();
-            String filePath = file.getAbsolutePath();
+				bufferedWriter.write(textArea.getText());
 
-            try {
-                FileReader fileReader = new FileReader(filePath);
-                BufferedReader bufferedReader = new BufferedReader(fileReader);
+				bufferedWriter.close();
+				fileWriter.close();
 
-                String line;
-                StringBuilder content = new StringBuilder();
-                while ((line = bufferedReader.readLine()) != null) {
-                    content.append(line).append("\n");
-                }
+				JOptionPane.showMessageDialog(frame, "Arquivo salvo com sucesso!");
+			} catch (IOException ex) {
+				JOptionPane.showMessageDialog(frame, "Ocorreu um erro ao salvar o arquivo.", "Erro",
+						JOptionPane.ERROR_MESSAGE);
+			}
+		}
+	}
 
-                bufferedReader.close();
-                fileReader.close();
+	private static void abrirArquivo() {
+		JFileChooser fileChooser = new JFileChooser();
+		int result = fileChooser.showOpenDialog(frame);
 
-                textArea.setText(content.toString());
-                JOptionPane.showMessageDialog(frame, "Arquivo aberto com sucesso!");
-            } catch (IOException ex) {
-                JOptionPane.showMessageDialog(frame, "Ocorreu um erro ao abrir o arquivo.", "Erro",
-                        JOptionPane.ERROR_MESSAGE);
-            }
-        }
-    }
+		if (result == JFileChooser.APPROVE_OPTION) {
+			File file = fileChooser.getSelectedFile();
+			String filePath = file.getAbsolutePath();
 
-    private static void criarNovoArquivo() {
-        textArea.setText("");
-    }
+			try {
+				FileReader fileReader = new FileReader(filePath);
+				BufferedReader bufferedReader = new BufferedReader(fileReader);
 
-    private static void limparTextArea() {
-        textArea.setText("");
-    }
+				String line;
+				StringBuilder content = new StringBuilder();
+				while ((line = bufferedReader.readLine()) != null) {
+					content.append(line).append("\n");
+				}
 
-    private static void fecharPrograma() {
-        int option = JOptionPane.showConfirmDialog(frame, "Deseja salvar o arquivo antes de fechar o programa?",
-                "Salvar", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE);
+				bufferedReader.close();
+				fileReader.close();
 
-        if (option == JOptionPane.YES_OPTION) {
-            salvarArquivo();
-            System.exit(0);
-        } else if (option == JOptionPane.NO_OPTION) {
-            System.exit(0);
-        }
-    }
+				textArea.setText(content.toString());
+				JOptionPane.showMessageDialog(frame, "Arquivo aberto com sucesso!");
+			} catch (IOException ex) {
+				JOptionPane.showMessageDialog(frame, "Ocorreu um erro ao abrir o arquivo.", "Erro",
+						JOptionPane.ERROR_MESSAGE);
+			}
+		}
+	}
+
+	private static void criarNovoArquivo() {
+		textArea.setText("");
+	}
+
+	private static void limparTextArea() {
+		textArea.setText("");
+	}
+
+	private static void fecharPrograma() {
+		int option = JOptionPane.showConfirmDialog(frame, "Deseja salvar o arquivo antes de fechar o programa?",
+				"Salvar", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE);
+
+		if (option == JOptionPane.YES_OPTION) {
+			salvarArquivo();
+			System.exit(0);
+		} else if (option == JOptionPane.NO_OPTION) {
+			System.exit(0);
+		}
+	}
 }
